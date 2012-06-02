492527bf4b0a76dca52ac8667df735441b068af9
DKRetzlaff <danielkretzlaff@gmail.com>
2012-06-01 21:58:20 -0300

    Update code/Evento.java

--- code/Evento.java
+++ code/Evento.java
@@ -1,76 +1,89 @@
-﻿public class Evento {
+﻿public class Evento
+﻿{
 	protected double data;
 	protected double duracao;
 	protected double desvioPadrao;
 
 
-	public Evento(){
+	public Evento()
+	{
 		this.data = 0.0;
 		this.duracao = 0.0;
 		this.desvioPadrao = 0.0;
 	}
 
 
-	public Evento(Evento E){
+	public Evento(Evento E)
+	{
 		this.data = E.data;
 		this.duracao = E.duracao;
 		this.desvioPadrao = E.desvioPadrao;
 	}
 
 
-	public Evento(double data, double duracao, double desvioPadrao){
+	public Evento(double data, double duracao, double desvioPadrao)
+	{
 		this.data = data;
 		this.duracao = duracao;
 		this.desvioPadrao = desvioPadrao;
 	}
 
 
-	public Evento(double data, double duracao){
+	public Evento(double data, double duracao)
+	{
 		this();
 		this.data = data;
 		this.duracao = duracao;
 	}
 
 
-	public void execucao(){
+	public void execucao()
+	{
 
 
 	}
 
 
-	public Evento gerarProximo(){
+	public Evento gerarProximo()
+	{
 		Evento temp = new Evento();
 		/* Alterar */
 		return temp;
 	}
 
 
-	public final void setData(double data){
+	public final void setData(double data)
+	{
 		this.data = data;
 	}
 
 
-	public final void setDuracao(double duracao){
+	public final void setDuracao(double duracao)
+	{
 		this.duracao = duracao;
 	}
 
 
-	public final void setDesvioPadrao(double desvioPadrao){
+	public final void setDesvioPadrao(double desvioPadrao)
+	{
 		this.desvioPadrao = desvioPadrao;
 	}
 
 
-	public final double getData(){
+	public final double getData()
+	{
 		return this.data;
 	}
 
 
-	public final double getDuracao(){
+	public final double getDuracao()
+	{
 		return this.duracao;
 	}
 
 
-	public final double getDesvioPadrao(){
+	public final double getDesvioPadrao()
+	{
 		return this.desvioPadrao;
 	}
 }
