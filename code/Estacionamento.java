eb33f4bf5047e3fc97613ed342bb9d3a11df18fd
andreguipeil <andreguipeil@gmail.com>
2012-06-01 21:00:00 -0300

    Update code/Estacionamento.java

--- code/Estacionamento.java
+++ code/Estacionamento.java
@@ -1,14 +1,60 @@
+
 public class Estacionamento {
 
-	private boolean vagas[];
+    // vetor boolean de vagas do estacionamento
+    private boolean vagas[];
 
-	public Estacionamento(int n) {
-		vagas = new boolean[n];
-	}
+    /**
+     * Construtor Default
+     */
+    public Estacionamento() {
+        vagas = new boolean[10];
+    }
 
-	public Estacionamento() {
-		vagas = new boolean[10];
-	}
 
-	 
+    /**
+     * Construtor com parametro
+     * @param n -> representa o numero de vagas do estacionamento
+     */
+    public Estacionamento(int n) {
+        vagas = new boolean[n];
+}
+
+
+    /**
+     * getVaga
+     * @param n
+     * @return retorna a vaga (V - F) na posicao n
+     */
+    public boolean getVaga(int n){
+    
+    return this.vagas[n];
+    }
+
+    /**
+     * altera o valor do vetor vagas na posicao n
+     * @param n
+     */
+    public void setVaga(int n){
+
+        this.vagas[n] = true;
+
+    }
+
+    /**
+     * verifica quantas vagas estao diponiveis
+     * @param n -> numero de vagas total
+     * @return numero de vagas disponiveis
+     */
+    public int vagaSobrando(int n){
+        int i, cont = 0;
+
+        for (i = 0; i < n; i++){
+            if (vagas[i])
+                cont++;
+        }
+        
+     return cont;
+    }
+
 }
