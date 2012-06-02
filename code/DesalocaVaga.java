18c8cd0eb17929bfa6c56e6188b4c09cf31d4065
DKRetzlaff <danielkretzlaff@gmail.com>
2012-06-01 21:50:16 -0300

    Identação

--- code/DesalocaVaga.java
+++ code/DesalocaVaga.java
@@ -1,11 +1,11 @@
 public class DesalocaVaga extends Evento{
 	
 	public static void main(int n){
-    	Desaloca( n );
+    		Desaloca( n );
 	}
 	
 	public static void Desaloca(int n){
-    	Estacionamento.vagas[n] = false;
+    		Estacionamento.vagas[n] = false;
 	}
 	
 	public void Desaloca(){
