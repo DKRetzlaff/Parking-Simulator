303cad16bc0da43795a6c8b87fc6f1cbb2f83319
DKRetzlaff <dkretzlaff@gmail.com>
2012-06-01 21:42:35 -0300

    Estatisca

---
+++ code/Estatistica.java
@@ -0,0 +1,22 @@
+public class Estatistica{
+	public int numeroVagas;
+	public float duracaoSimulacao;
+	public int numeroTotalRequisicaoVagas;
+	public int numeroFalhasEmVagasOcupadas;			//numero de vagas que naum poderam ser ocupadas por estarem todas ocupadas
+	
+	public Estatistica(){
+		numeroVagas = 0;
+		duracaoSimulacao = 0;
+		numeroTotalRequisicaoVagas = 0;
+		numeroFalhasEmVagasOcupadas = 0;
+	}
+	
+	public Estatistica(int numeroVagas, float duracaoSimulacao, int numeroTotalRequisicaoVagas, int numeroFalhasEmVagasOcupadas){
+		this.numeroVagas = numeroVagas;
+		this.duracaoSimulacao = duracaoSimulacao;
+		this.numeroTotalRequisicaoVagas = numeroTotalRequisicaoVagas;
+		this.numeroFalhasEmVagasOcupadas = numeroFalhasEmVagasOcupadas;
+	}
+	
+	
+}
