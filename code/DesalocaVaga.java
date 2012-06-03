79b494352f3c6c1e47f5a6ce543a1fc112c42d39
andreguipeil <andreguipeil@gmail.com>
2012-06-03 20:43:32 -0300

    detalezinho

--- code/DesalocaVaga.java
+++ code/DesalocaVaga.java
@@ -1,22 +1,24 @@
-import java.util.Random
+import java.util.Random;
 
 public class DesalocaVaga extends Evento{
-	
-	private Random aleatorio;
+
+private Random aleatorio;
 
     public DesalocaVaga(double data, double duracao){
         this.data = data;
         this.duracao = duracao;
     }
 
+    @Override
     public void execucao(){
     }
     
+    @Override
     public Evento gerarProximo(){
-		double hora;
-		aleatorio = new Random();
-		hora = aleatorio.nextDouble(120);
-        Evento e = new AlocaVaga(this.getData(), random);
+        double hora;
+            aleatorio = new Random();
+            hora = aleatorio.nextDouble();
+        Evento e = new AlocaVaga(this.getData(), hora);
         return e;
     }
 }
