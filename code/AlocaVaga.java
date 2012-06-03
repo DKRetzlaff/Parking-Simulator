8b4b39b38311efa84a385d280aa5de414abb8d81
DKRetzlaff <danielkretzlaff@gmail.com>
2012-06-03 11:48:47 -0300

    Comentários adicionados

--- code/AlocaVaga.java
+++ code/AlocaVaga.java
@@ -1,6 +1,6 @@
 public class AlocaVaga extends Evento{
 
-    //Troca o valor para true ou falso da posicao x
+    //Troca o valor para true ou falso da posicao x;
     public void estaciona(Estacionamento setar, int x)
     {
             setar.setVaga(x);
